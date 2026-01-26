19e1c858d38a3e5ad4f806bdbb08550857cbcd8d
1brucben <1benjbruce@gmail.com>
2026-01-26 12:35:04 +0100

    feat: update minimum distance for structure placement to 15

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -1274,8 +1274,7 @@ export class DefaultConfig implements Config {
   }
 
   structureMinDist(): number {
-    // TODO: Increase this to ~15 once upgradable structures are implemented.
-    return 1;
+    return 15;
   }
 
   shellLifetime(): number {
